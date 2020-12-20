d061e6b00d44593207c514550a92128743551e7b
Asad Tariq <51799278+asadtariq18@users.noreply.github.com>
2020-12-20 18:17:17 +0500

    Update HistoryScreen.js
    clear all button added

--- HistoryScreen.js
+++ HistoryScreen.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   View,
@@ -6,48 +6,62 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  Dimensions,
-  Image
-  
+  Button,
 } from 'react-native';
-import Constants from 'expo-constants';
-const screenWidth = Math.round(Dimensions.get('window').width);
-const screenHeight = Math.round(Dimensions.get('window').height);
 
-
-
-export default function HistoryScreen({route, navigation}) {
-
-const { list, setList  } = route.params;
+export default function HistoryScreen({ route, navigation }) {
+  const { list, setList } = route.params;
 
   const removeHandler = (key) => {
     setList((prevList) => {
-      return prevList.filter(list => list.key != key)
-    })
-    navigation.navigate('Home')
-    alert('removed')
-    }
+      return prevList.filter((list) => list.key != key);
+    });
+    navigation.navigate('Home');
+    alert('removed');
+  };
 
+  const clearHistory = () => {
+    setList([]);
+    navigation.navigate('Home');
+    alert('History Cleared');
+  };
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => clearHistory()} title="Clear History" />
+      ),
+    });
+  }, [navigation, list]);
 
-return(
-   <SafeAreaView style={styles.container}>
-   <FlatList
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+        <Text style={styles.title}> Original Price </Text>
+        <Text style={styles.title}> Final Price </Text>
+        <Text style={styles.title}> You Save </Text>
+      </View>
+      <FlatList
+        style={{ flexDirection: 'row' }}
         data={list}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress ={() => removeHandler(item.key)}>
-            <View style={styles.item}>
-              <Text style={styles.nameText}>Original Price: {item.orgPrice}</Text>
-              <Text style={styles.text}>You Save: {item.savePrice}</Text>
-              <Text style={styles.text}>Final Price: {item.finalPrice}</Text>
-              </View>
-              </TouchableOpacity>
+          <TouchableOpacity onPress={() => removeHandler(item.key)}>
+            <View
+              style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+              <Text style={styles.text}> {item.orgPrice} </Text>
+              <Text style={styles.text}> {item.finalPrice} </Text>
+              <Text style={styles.text}> {item.savePrice} </Text>
+            </View>
+          </TouchableOpacity>
         )}
         keyExtractor={(item) => item.id}
       />
-      <Text style={{color: '#888', marginBottom:10, marginLeft:110}}> Touch any item to remove </Text>
-       </SafeAreaView>
-
-)}
+      <Text style={{ color: '#888', marginBottom: 10, marginLeft: 110 }}>
+        {' '}
+        Touch any item to remove{' '}
+      </Text>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -56,29 +70,18 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 15,
   },
-    header: {
-    width: '100%',
-    justifyContent: 'space-around',
-    flexDirection: 'column',
-    margin: 20,
-    paddingBottom: 10,
-  },
-  headerText: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'Black',
-  },
-  nameText: {
+  title: {
+    paddingHorizontal: 30,
     color: 'dodgerblue',
     fontWeight: 'bold',
-    fontSize: 25,
-    padding: 10,
-    marginLeft:10
-  },
-    text: {
-    color: '#888',
-    fontWeight: 'bold',
     fontSize: 20,
-    marginLeft:20
+    padding: 20,
+  },
+  text: {
+    paddingHorizontal: 30,
+    paddingBottom: 20,
+    fontWeight: 'bold',
+    fontSize: 25,
+    marginLeft: 20,
   },
 });
